3cc8cd3d9fcfa03c5b31805aaf0b3ed92021c857
oviedev <ovienathaniel44@gmail.com>
2025-12-25 18:38:39 +0100

    completed refernces and brorrowing project

---
+++ references-and-borrowing-project/target/debug/deps/references_and_borrowing_project-36c18866c0073e01.d
@@ -0,0 +1,5 @@
+/home/nathaniel/RUST/references-and-borrowing-project/target/debug/deps/references_and_borrowing_project-36c18866c0073e01.d: src/main.rs
+
+/home/nathaniel/RUST/references-and-borrowing-project/target/debug/deps/references_and_borrowing_project-36c18866c0073e01: src/main.rs
+
+src/main.rs:
---
+++ references-and-borrowing-project/target/debug/references-and-borrowing-project.d
@@ -0,0 +1,1 @@
+/home/nathaniel/RUST/references-and-borrowing-project/target/debug/references-and-borrowing-project: /home/nathaniel/RUST/references-and-borrowing-project/src/main.rs
