fc826fc25b183751810c97d16280aef99eae82ad
oviedev <ovienathaniel44@gmail.com>
2025-12-26 06:48:44 +0100

    did slices project

---
+++ slices-project/target/debug/deps/slices_project-0addd28610a7471b.d
@@ -0,0 +1,5 @@
+/home/nathaniel/RUST/slices-project/target/debug/deps/slices_project-0addd28610a7471b.d: src/main.rs
+
+/home/nathaniel/RUST/slices-project/target/debug/deps/slices_project-0addd28610a7471b: src/main.rs
+
+src/main.rs:
---
+++ slices-project/target/debug/slices-project.d
@@ -0,0 +1,1 @@
+/home/nathaniel/RUST/slices-project/target/debug/slices-project: /home/nathaniel/RUST/slices-project/src/main.rs
